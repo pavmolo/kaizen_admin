f1591c6583e86940089d6e5aeff1abf6376168ea
pavmolo <74321783+pavmolo@users.noreply.github.com>
2023-09-06 21:16:04 +0300

    Update kaizen_admin.py

--- kaizen_admin.py
+++ kaizen_admin.py
@@ -7,7 +7,7 @@ import psycopg2
 DATABASE_CONFIG = {
     'dbname': 'kaizen_it_administration_aport',
     'user': 'pavmolo',
-    'password': st.secrets["database"]["password"],  # Используем секретный пароль из secrets.toml 
+    'password': st.secrets["database"]["password"],
     'host': '45.86.182.111'
 }
 
@@ -15,32 +15,30 @@ def get_connection():
     """Получение соединения с базой данных."""
     return psycopg2.connect(**DATABASE_CONFIG)
 
-def get_databases():
-    """Получение списка баз данных."""
+def create_table(table_name, fields):
+    """Создание таблицы с указанными полями."""
     with get_connection() as conn:
         with conn.cursor() as cursor:
-            cursor.execute("SELECT datname FROM pg_database;")
-            return [row[0] for row in cursor.fetchall()]
+            fields_str = ", ".join([f"{name} {type}" for name, type in fields])
+            cursor.execute(f"CREATE TABLE {table_name} ({fields_str});")
+        conn.commit()
 
-def get_tables(database):
-    """Получение списка таблиц для указанной базы данных."""
-    with get_connection() as conn:
-        with conn.cursor() as cursor:
-            cursor.execute(f"SELECT tablename FROM pg_tables WHERE schemaname='public';")
-            return [row[0] for row in cursor.fetchall()]
+st.title("Управление базой данных")
 
-def get_table_data(table_name):
-    """Получение данных из указанной таблицы."""
-    with get_connection() as conn:
-        return pd.read_sql(f"SELECT * FROM {table_name};", conn)
+# Создание новой таблицы
+st.subheader("Создание новой таблицы")
 
-st.title("Просмотр баз данных и таблиц")
+table_name = st.text_input("Имя таблицы")
 
-# Выбор базы данных
-database = st.selectbox("Выберите базу данных:", get_databases())
+fields = []
+field_count = st.number_input("Количество полей", min_value=1, max_value=10, value=1)
 
-# Выбор таблицы
-table = st.selectbox("Выберите таблицу:", get_tables(database))
+for i in range(field_count):
+    st.subheader(f"Поле {i+1}")
+    field_name = st.text_input(f"Имя поля {i+1}", key=f"field_name_{i}")
+    field_type = st.selectbox(f"Тип поля {i+1}", ["INTEGER", "VARCHAR", "TEXT", "DATE", "FLOAT"], key=f"field_type_{i}")
+    fields.append((field_name, field_type))
 
-# Отображение данных таблицы
-st.write(get_table_data(table))
+if st.button("Создать таблицу"):
+    create_table(table_name, fields)
+    st.success(f"Таблица {table_name} успешно создана!")
